9b9ffb4ef81ff6bf3ded16013793d58abd81e029
savaged <savagedavid@gmx.com>
2022-08-15 10:47:24 +0100

    mocking done for this scenario

--- Bunsen.Test/FeatureTests/StandardUserAddsScenarioStepLog.cs
+++ Bunsen.Test/FeatureTests/StandardUserAddsScenarioStepLog.cs
@@ -1,6 +1,10 @@
 using Bunsen.API;
 using Bunsen.Models;
 using Moq;
+using Moq.Dapper;
+using Dapper;
+using Bunsen.Data;
+using System.Data.Common;
 
 namespace Bunsen.Test.FeatureTests;
 
@@ -12,22 +16,35 @@ public class FeatureScenarioStepLogCRUD
     [TestInitialize]
     public void Init()
     {
-        // TODO switch to mocking the dbconnection
-        Mock<IDataService> mock = new Mock<IDataService>();
-        mock.Setup(s => s.StoreAsync(It.IsAny<ScenarioStepLog>()).Result).Returns<ScenarioStepLog>(m => m);
-        _dataService = mock.Object;
+        var mockDbConnection = new Mock<DbConnection>();
+        mockDbConnection.SetupDapperAsync(
+            c => c.ExecuteAsync(It.IsAny<string>(), It.IsAny<ScenarioStepLog>(), null, null, null))
+            .ReturnsAsync(1);
+        mockDbConnection.SetupDapperAsync(c => c.QuerySingleOrDefaultAsync<ScenarioStepLog>(
+            It.IsAny<string>(), null, null, null, null))
+            .ReturnsAsync(GetNewModel());
+
+        _dataService = new DataService(mockDbConnection.Object);
     }
 
     [TestMethod]
     public async Task StandardUserAddsScenarioStepLog()
     {
-        var model = new ScenarioStepLog
+        var model = GetNewModel();
+        var saved = await _dataService?.StoreAsync(model)!;
+        Assert.AreEqual(model.Id, saved.Id);
+        Assert.AreEqual(model.Name, saved.Name);
+        Assert.AreEqual(model.StartOfStep, saved.StartOfStep);
+        Assert.AreEqual(model.EndOfStep, saved.EndOfStep);
+    }
+
+    private ScenarioStepLog GetNewModel()
+    {
+        return new ScenarioStepLog
         {
             Name = "TestStep1",
             StartOfStep = new DateTime(2022, 8, 14, 8, 25, 30),
             EndOfStep = new DateTime(2022, 8, 14, 8, 26, 18),
         };
-        var saved = await _dataService?.StoreAsync(model)!;
-        Assert.AreEqual(model, saved);
     }
 }
