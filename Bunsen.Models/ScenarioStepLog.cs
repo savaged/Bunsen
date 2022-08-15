52b1b9874693777408ddcd3150de082df92938d7
savaged <savagedavid@gmail.com>
2022-08-15 08:45:55 +0100

    Basic unit test

--- Bunsen.Models/ScenarioStepLog.cs
+++ Bunsen.Models/ScenarioStepLog.cs
@@ -1,9 +1,12 @@
-﻿namespace Bunsen.Models
+﻿using System;
+
+namespace Bunsen.Models
 {
     public class ScenarioStepLog : ModelBase
     {
         private string _name = string.Empty;
-
+        private DateTime? _startOfStep;
+        private DateTime? _endOfStep;
         private bool _isPassing;
 
         public string Name
@@ -12,10 +15,28 @@
             set => _name = value;
         }
 
+        public DateTime? StartOfStep
+        {
+            get => _startOfStep;
+            set => _startOfStep = value;
+        }
+        
+        public DateTime? EndOfStep
+        {
+            get => _endOfStep;
+            set => _endOfStep = value;
+        }
+
         public bool IsPassing
         {
             get => _isPassing;
             set => _isPassing = value;
         }
+
+        public double CalculateSecondsTakenOfStep()
+        {
+            return (EndOfStep - StartOfStep)?.TotalSeconds ?? 0;
+        }
+        
     }
 }
