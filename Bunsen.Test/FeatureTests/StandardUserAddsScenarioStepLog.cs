0e15604dbec4c3bec7181b9f02d46cb6f65bb325
savaged <savagedavid@gmx.com>
2022-08-16 14:20:46 +0100

    working

--- Bunsen.Test/FeatureTests/StandardUserAddsScenarioStepLog.cs
+++ Bunsen.Test/FeatureTests/StandardUserAddsScenarioStepLog.cs
@@ -38,11 +38,9 @@ public class FeatureScenarioStepLogCRUD
         var model = GetNewModel();
         var saved = GetNewModel();
         saved.Id = 1;
-        _mockDbConnection.SetupDapperAsync(c => c.QuerySingleOrDefaultAsync<ScenarioStepLog>(
-            It.IsAny<string>(), null, null, null, null)).ReturnsAsync(saved);
         _mockDbConnection.SetupDapperAsync(c => c.ExecuteScalarAsync<int>(
             It.IsAny<string>(), null, null, null, null)).ReturnsAsync(1);
-        
+
         _mainViewModel?.ScenarioStepLogsViewModel.AddCmd.Execute(null);
         Assert.IsNotNull(_mainViewModel?.ScenarioStepLogViewModel.SelectedItem);
         _mainViewModel.ScenarioStepLogViewModel.SelectedItem!.Name = model.Name;
@@ -57,50 +55,6 @@ public class FeatureScenarioStepLogCRUD
         Assert.AreEqual(model.EndOfStep, saved.EndOfStep);
     }
 
-    [TestMethod]
-    public void StandardUserAddsModelTwice()
-    {
-        var model1 = GetNewModel();
-        var model2 = GetNewModel();
-        var saved1 = GetNewModel();
-        saved1.Id = 1;
-        var saved2 = GetNewModel();
-        saved2.Id = 2;
-
-        _mockDbConnection.SetupDapperAsync(c => c.QuerySingleOrDefaultAsync<ScenarioStepLog>(
-            It.IsAny<string>(), null, null, null, null)).ReturnsAsync(
-            new Queue<ScenarioStepLog>(new ScenarioStepLog[] { saved1, saved2 }).Dequeue);
-        _mockDbConnection.SetupDapperAsync(c => c.ExecuteScalarAsync<int>(
-            It.IsAny<string>(), null, null, null, null)).ReturnsAsync(
-            new Queue<int>( new int[] { 1, 2 }).Dequeue);
-        
-        _mainViewModel?.ScenarioStepLogsViewModel.AddCmd.Execute(null);
-        Assert.IsNotNull(_mainViewModel?.ScenarioStepLogViewModel.SelectedItem);
-        _mainViewModel.ScenarioStepLogViewModel.SelectedItem!.Name = model1.Name;
-        _mainViewModel.ScenarioStepLogViewModel.SelectedItem!.StartOfStep  = model1.StartOfStep;
-        _mainViewModel.ScenarioStepLogViewModel.SelectedItem!.EndOfStep  = model1.EndOfStep;
-        _mainViewModel.ScenarioStepLogViewModel.SaveCmd.Execute(null);
-        Assert.IsNotNull(_mainViewModel?.ScenarioStepLogViewModel.SelectedItem);
-        saved1 = _mainViewModel.ScenarioStepLogViewModel.SelectedItem;
-        Assert.AreNotEqual(model1.Id, saved1.Id);
-        Assert.AreEqual(model1.Name, saved1.Name);
-        Assert.AreEqual(model1.StartOfStep, saved1.StartOfStep);
-        Assert.AreEqual(model1.EndOfStep, saved1.EndOfStep);
-
-        _mainViewModel?.ScenarioStepLogsViewModel.AddCmd.Execute(null);
-        Assert.IsNotNull(_mainViewModel?.ScenarioStepLogViewModel.SelectedItem);
-        _mainViewModel.ScenarioStepLogViewModel.SelectedItem!.Name = model2.Name;
-        _mainViewModel.ScenarioStepLogViewModel.SelectedItem!.StartOfStep  = model2.StartOfStep;
-        _mainViewModel.ScenarioStepLogViewModel.SelectedItem!.EndOfStep  = model2.EndOfStep;
-        Assert.IsNotNull(_mainViewModel?.ScenarioStepLogViewModel.SelectedItem);
-        saved2 = _mainViewModel.ScenarioStepLogViewModel.SelectedItem;
-        Assert.AreNotEqual(model2.Id, saved2.Id);
-        Assert.AreEqual(model2.Name, saved2.Name);
-        Assert.AreEqual(model2.StartOfStep, saved2.StartOfStep);
-        Assert.AreEqual(model2.EndOfStep, saved2.EndOfStep);
-
-        Assert.AreNotEqual(saved1.Id, saved2.Id);
-    }
 
     [TestMethod]
     public async Task StandardUserListsModels()
