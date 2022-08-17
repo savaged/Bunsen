57addf643cb10c2f8d8ca138cf993df430798ae4
savaged <savagedavid@gmx.com>
2022-08-17 15:47:00 +0100

    formatting feature test to be BDD like

--- Bunsen.Test/FeatureTests/Feature__ScenarioStepLog_CRUD.cs
+++ Bunsen.Test/FeatureTests/Feature__ScenarioStepLog_CRUD.cs
@@ -10,41 +10,34 @@ using Bunsen.ViewModels.Core;
 namespace Bunsen.Test.FeatureTests;
 
 [TestClass]
-public class FeatureScenarioStepLogCRUD
+public class Feature__ScenarioStepLog_CRUD
 {
     private Mock<DbConnection>? _mockDbConnection;
     private MainViewModel? _mainViewModel;
     
+    /// <summary>
+    /// Note:
+    /// 
+    ///     Given definition of 'not new' is the ScenarioStepLog.Id is greater than 0
+    ///
+    /// </summary>
     [TestInitialize]
-    public void Init()
+    public void Background__()
     {
-        _mockDbConnection = new Mock<DbConnection>();
-        _mockDbConnection.SetupDapperAsync(
-            c => c.ExecuteAsync(It.IsAny<string>(), It.IsAny<ScenarioStepLog>(), null, null, null))
-            .ReturnsAsync(1);
-        _mockDbConnection.SetupDapperAsync(c => c.QuerySingleOrDefaultAsync<ScenarioStepLog>(
-            It.IsAny<string>(), null, null, null, null)).ReturnsAsync(GetNewModel());
-        _mockDbConnection.SetupDapperAsync(c => c.ExecuteAsync(
-            It.IsAny<string>(), It.IsAny<ScenarioStepLog>(), null, null, null));
-
-        _mainViewModel = new MainViewModel(
-            BusyStateManager.Instance,
-            new DataService(_mockDbConnection.Object));
+        Given_a_ScenarioStepLog_database_is_loaded();
+        Given_the_Main_View_is_open();
     }
 
     [TestMethod]
-    public void StandardUserAddsModel()
+    public void Scenario__A_Standard_User_adds_a_ScenarioStepLog()
     {
-        // Given
-        Assert.IsNull(_mainViewModel?.ScenarioStepLogViewModel.SelectedItem);
-        // When
-        _mainViewModel?.ScenarioStepLogsViewModel.AddCmd.Execute(null);
-        // Then
-        Assert.IsNotNull(_mainViewModel?.ScenarioStepLogViewModel.SelectedItem);
+        When_a_Standard_User_clicks_Add();
+
+        Then_a_new_ScenarioStepLog_is_available();
     }
 
     [TestMethod]
-    public void StandardUserSavesModel()
+    public void StandardUserSavesANewModel()
     {
         // Given
         var model = GetNewModel();
@@ -59,6 +52,7 @@ public class FeatureScenarioStepLogCRUD
         // Then
         Assert.IsNotNull(_mainViewModel.ScenarioStepLogViewModel.SelectedItem);
         var saved = _mainViewModel.ScenarioStepLogViewModel.SelectedItem;
+        Assert.AreEqual(1, saved.Id);
         Assert.AreEqual(model.Name, saved.Name);
         Assert.AreEqual(model.StartOfStep, saved.StartOfStep);
         Assert.AreEqual(model.EndOfStep, saved.EndOfStep);
@@ -110,6 +104,41 @@ public class FeatureScenarioStepLogCRUD
         _mainViewModel?.ScenarioStepLogViewModel.DeleteCmd.Execute(null);
         // Then
         Assert.IsNull(_mainViewModel?.ScenarioStepLogViewModel.SelectedItem);
+    }
+
+
+    private void Given_a_ScenarioStepLog_database_is_loaded()
+    {
+        _mockDbConnection = new Mock<DbConnection>();
+        _mockDbConnection.SetupDapperAsync(
+            c => c.ExecuteAsync(It.IsAny<string>(), It.IsAny<ScenarioStepLog>(), null, null, null))
+            .ReturnsAsync(1);
+        _mockDbConnection.SetupDapperAsync(c => c.QuerySingleOrDefaultAsync<ScenarioStepLog>(
+            It.IsAny<string>(), null, null, null, null)).ReturnsAsync(GetNewModel());
+        _mockDbConnection.SetupDapperAsync(c => c.ExecuteAsync(
+            It.IsAny<string>(), It.IsAny<ScenarioStepLog>(), null, null, null));
+    }
+
+    private void Given_the_Main_View_is_open()
+    {
+        if (_mockDbConnection != null)
+        {
+            _mainViewModel = new MainViewModel(
+                BusyStateManager.Instance,
+                new DataService(_mockDbConnection.Object));
+        }
+    }
+
+
+    private void When_a_Standard_User_clicks_Add()
+    {
+        _mainViewModel?.ScenarioStepLogsViewModel.AddCmd.Execute(null);
+    }
+
+
+    private void Then_a_new_ScenarioStepLog_is_available()
+    {
+        Assert.IsNotNull(_mainViewModel?.ScenarioStepLogViewModel.SelectedItem);
     }
 
 
